5892093f752b8019847a86388d437cb99d1ddc06
phantomsquid0928 <phantomsquid0928@gmail.com>
2024-10-27 10:26:05 +0900

    fix: before sigmoid output is only negative value, so he init added <- should be applied to other models

--- simple_glaucomacnn.py
+++ simple_glaucomacnn.py
@@ -248,13 +248,14 @@ class SimpleConvNet:
 
             if name == 'out':
                 # Only single convolution for output
-                W = weight_init_std * np.random.randn(vals['filter_out'], vals['filter_in'], vals['filter_size'], vals['filter_size'])
+                #added he init
+                W = np.random.randn(vals['filter_out'], vals['filter_in'], vals['filter_size'], vals['filter_size']) * np.sqrt(2.0 / vals['filter_in'])
                 b = np.zeros(vals['filter_out'])
                 self.params[f'W_{name}'] = W
                 self.params[f'b_{name}'] = b
                 self.layers[name] = Convolution(W, b, vals['stride'], vals['pad'])
             else:
-                W1 = weight_init_std * np.random.randn(vals['filter_out'], vals['filter_in'], vals['filter_size'], vals['filter_size'])
+                W1 = np.random.randn(vals['filter_out'], vals['filter_in'], vals['filter_size'], vals['filter_size']) * np.sqrt(2.0 / vals['filter_in'])
                 # W2 = weight_init_std * np.random.randn(vals['filter_out'], vals['filter_out'], vals['filter_size'], vals['filter_size'])
                 b1 = np.zeros(vals['filter_out'])
                 # b2 = np.zeros(vals['filter_out'])
